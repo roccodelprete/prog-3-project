23518ed2dee6dcd0a93df2538c72328aa58c04e7
Rocco Del Prete <122788299+roccodelprete@users.noreply.github.com>
2023-12-30 18:43:49 +0100

    add possibility to change route vehicle

--- SistemaSicve/src/main/java/app/Test.java
+++ SistemaSicve/src/main/java/app/Test.java
@@ -2,40 +2,31 @@ package app;
 
 import command.pattern.*;
 import observer_memento.pattern.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 /**
- * Test class
+ * Test class to test the application
  * @author Rocco Del Prete
  */
 public class Test {
     /**
      * function to move a vehicle on a route
-     * @param vehicle The vehicle to move
-     * @param route The route where the vehicle is moving
-     * @param tutorSystem The tutor system to detect infractions
-     * @param entranceStation The entrance station
-     * @param exitStation The exit station
-     * @param policeStation The police station to send infractions
+     * @param vehicle                   The vehicle to move
+     * @param route                     The route where the vehicle is moving
+     * @param tutorSystem               The tutor system to detect infractions
+     * @param tripLength                The length of the trip
      */
-    private static void moveVehicleOnRoute(Vehicle vehicle, Route route, TutorSystem tutorSystem, TutorStation entranceStation, TutorStation exitStation, PoliceStation policeStation) {
-        entranceStation.attach(vehicle);
-        entranceStation.notifyObserver(vehicle, "entered the route with tutor system\n");
+    private static Double moveVehicleOnRoute(@NotNull Vehicle vehicle, @NotNull Route route, TutorSystem tutorSystem, Double tripLength) {
+        Double detectedSpeed = Math.random() * 130.0 + 60.0;
 
-        for (int i = 0; i < 4; i++) {
-            vehicle.moveVehicle();
-            tutorSystem.detectSpeed(Math.random() * 200.0 + 60.0, route, vehicle);
-        }
+        vehicle.moveVehicle();
+        tripLength -= vehicle.getCurrentMilestone();
 
-        tutorSystem.sendAllInfractions(vehicle, policeStation, route);
-        tutorSystem.sendMostSevereInfraction(policeStation, route, vehicle);
+        tutorSystem.detectSpeed(detectedSpeed, route, vehicle);
 
-        if (vehicle.getCurrentMilestone() >= route.getLength()) {
-            exitStation.attach(vehicle);
-            entranceStation.detach(vehicle);
-            exitStation.notifyObserver(vehicle, "exited the route with tutor system\n");
-        }
+        return tripLength;
     }
 
     public static void main(String[] args) {
@@ -43,7 +34,7 @@ public class Test {
         TutorSystem tutorSystem = new TutorSystem();
         PoliceStation policeStation = new PoliceStation("Police Station 1");
 
-        Route route1 = new Route("Route 1", 90.0, 1200.0);
+        Route route1 = new Route("Route 1", 90.0, 120.0);
         Route route2 = new Route("Route 2", 80.0, 1500.0);
 
         ArrayList<Command> commands = new ArrayList<>();
@@ -52,13 +43,21 @@ public class Test {
         commands.add(new AddRouteCommand(tutorSystem, route2));
 
         commands.forEach(admin::addCommand);
-        admin.executeCommands();
+        commands.forEach(admin::executeCommand);
+
+        Route currentRoute = tutorSystem.getRoutes().getFirst();
 
         TutorStation entranceStation = new TutorStation();
         TutorStation exitStation = new TutorStation();
 
         Vehicle vehicle1 = new Vehicle("ABC123", "Fiat", "Punto");
         Vehicle vehicle2 = new Vehicle("DEF456", "Fiat", "Panda");
+
+        double tripLength = 1000.0;
+
+        entranceStation.attach(vehicle1);
+        entranceStation.notifyObserver(vehicle1, "entered the route " + currentRoute.getName() + "\n");
+        entranceStation.attach(vehicle2);
 
         ArrayList<Command> statisticsCommands = new ArrayList<>();
         statisticsCommands.add(new GetRouteVehicleSpeedStatisticsCommand(tutorSystem, route1, vehicle1));
@@ -70,8 +69,31 @@ public class Test {
 
         statisticsCommands.forEach(admin::addCommand);
 
-        moveVehicleOnRoute(vehicle1, route1, tutorSystem, entranceStation, exitStation, policeStation);
-        moveVehicleOnRoute(vehicle2, route1, tutorSystem, entranceStation, exitStation, policeStation);
+        while (tripLength > 0.0) {
+            tripLength = moveVehicleOnRoute(vehicle1, currentRoute, tutorSystem, tripLength);
+
+            if (vehicle1.getCurrentMilestone() >= currentRoute.getLength()) {
+                exitStation.attach(vehicle1);
+
+                exitStation.notifyObserver(vehicle1, "exited the route " + currentRoute.getName() + "\n");
+
+                entranceStation.notifyObserver(vehicle1, "entered the route " + currentRoute.getName() + "\n");
+
+                currentRoute = tutorSystem.getRoutes().get(tutorSystem.getRoutes().indexOf(route2));
+            }
+
+            if (tripLength <= 0.0) {
+                System.out.println("Trip ended...\n");
+                break;
+            }
+        }
+
+        if (tutorSystem.getInfractions().containsKey(vehicle1)) {
+            tutorSystem.getInfractions().get(vehicle1).forEach((route, infractions) -> {
+                tutorSystem.sendAllInfractions(vehicle1, policeStation, route);
+                tutorSystem.sendMostSevereInfraction(policeStation, route, vehicle1);
+            });
+        }
 
         statisticsCommands.forEach(admin::executeCommand);
     }
